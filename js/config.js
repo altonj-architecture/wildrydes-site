98fe497a29c7f957dec7c67dbb1caf07cbd2c2ac
altonj-architecture <apolloj.jones1@gmail.com>
2024-10-24 14:35:46 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_rv0BRUTcL', 
         userPoolClientId: '3hiilpvgn2hvuk7s5j36h4cl2q', 
-        region: '' // e.g. us-east-1
+        region: 'us-east-1' 
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
